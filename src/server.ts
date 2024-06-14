69dcdbbbb5a47c69d9f587c70e502d311bc8409b
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-06-14 08:38:07 +0530

    swagger file updates

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import fs from "fs";
 import YAML from "yaml";
 import { adminRoutes, shoppingRoutes, vendorRoutes } from "./routes";
 import { database } from "./utils";
 import swaggerUi from "swagger-ui-express";
-
-const file = fs.readFileSync(path.resolve(__dirname, "./swagger.yaml"), "utf8");
+console.log(__dirname);
+const file = fs.readFileSync("./swagger.yaml", "utf8");
 const swaggerDocument = YAML.parse(
   file?.replace(
     "- url: ${{server}}",
@@ -26,13 +25,13 @@ app.use("/admin/", adminRoutes);
 app.use("/vendor/", vendorRoutes);
 
 app.use(
-  "/api/docs/",
+  "/api/docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
       docExpansion: "none",
     },
-    customSiteTitle: "FreeAPI docs",
+    customSiteTitle: "Food Fusion Api Documentation",
   })
 );
 
