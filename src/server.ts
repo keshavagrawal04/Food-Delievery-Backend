1f8e4cb60d0b12cbb1d04974231e4bb8aa972580
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-06-14 08:28:04 +0530

    swagger yaml added

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
+import YAML from "yaml";
 import { adminRoutes, shoppingRoutes, vendorRoutes } from "./routes";
 import { database } from "./utils";
 import swaggerUi from "swagger-ui-express";
-import { swagger } from "./configs";
-import path from "path";
+
+const file = fs.readFileSync(path.resolve(__dirname, "./swagger.yaml"), "utf8");
+const swaggerDocument = YAML.parse(
+  file?.replace(
+    "- url: ${{server}}",
+    `- url: ${process.env.FOOD_FUSION_HOST_URL}`
+  )
+);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/images", express.static(path.join(__dirname, "images")));
 
 database.connect();
 
@@ -20,7 +28,7 @@ app.use("/vendor/", vendorRoutes);
 app.use(
   "/api/docs/",
   swaggerUi.serve,
-  swaggerUi.setup(swagger.swaggerDocument, {
+  swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
       docExpansion: "none",
     },
